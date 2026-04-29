fc4f9c8eb472c6fc59a5b2c21657c624dd516243
Aadityaa Mehrotra <147915949+aadityaamehrotra17@users.noreply.github.com>
2026-04-29 23:51:52 +0100

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        set_s, set_t = {}, {}  # {'c':1, 'd':2} == {'d':2, 'c':1}
+
+        for i in s:
+            set_s[i] = set_s.get(i, 0) + 1
+        
+        for i in t:
+            set_t[i] = set_t.get(i, 0) + 1
+
+        return set_s == set_t
+
+    """
+    Space: O(n + m)
+    Time: O(n + m)
+    """
