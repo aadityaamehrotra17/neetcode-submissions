3ed502c7649c33191d1f67dbb0e4bfa92d4f77e0
Aadityaa Mehrotra <147915949+aadityaamehrotra17@users.noreply.github.com>
2026-04-29 23:26:37 +0100

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        num_set = set(nums)
+
+        return not (len(num_set) == len(nums))
+
+"""
+Space: O(n)
+Time: O(n)
+"""
